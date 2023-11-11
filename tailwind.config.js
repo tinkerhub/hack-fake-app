69a6ffb46688a10d048d7561732616ef557c316b
Abdul Wajid <abdulwajidnv@gmail.com>
2023-11-11 22:21:15 +0530

    build: extend palette colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#D36C31",
+                secondary: "#F1B898",
+                background: "#FEFEFE",
+                text: "#000000",
+                offtext: "#6E6E6E",
+            },
+        },
     },
     plugins: [require("daisyui")],
 };
