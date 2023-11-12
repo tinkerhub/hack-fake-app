6d249b1fadd5ff4ec1d476d287463361b8177fc8
S-HARI-S <sreeharisureshmltr@gmail.com>
2023-11-12 11:15:14 +0530

    feature

--- src/components/index.ts
+++ src/components/index.ts
@@ -23,5 +23,4 @@ export {
     Home,
     Feed,
     FeedLayout,
-
 };
--- src/components/pages/Feed.tsx
+++ src/components/pages/Feed.tsx
@@ -1,12 +1,16 @@
 import FeedTitle from "../interface/FeedTitle";
 import FeedLayout from "../layouts/FeedLayout";
 import ContentForm from "./feeds/ContentForm";
+import PredictAnnotateSelect from "./feeds/PredictAnnotateSelect";
+import Submit from "./feeds/AiOpinion";
 import TitleFrom from "./feeds/TitleFrom";
+import UrlForm from "./feeds/UrlForm";
 
 function Feed() {
     return (
+
         <FeedLayout>
-            <ContentForm />
+             <ContentForm />
         </FeedLayout>
     );
 }
